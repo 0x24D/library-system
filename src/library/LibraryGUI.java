dcde4691e1f67ae24170dd881b071fd46fe50498
0x24d <daniel.longfield@tuta.io>
2018-10-19 12:00:31 +0100

    Remove empty template

--- src/library/LibraryGUI.java
+++ src/library/LibraryGUI.java
@@ -1,13 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
- /*
- * LibraryGUI.java
- *
- * Created on 28-Sep-2009, 11:55:39
- */
 package library;
 
 import java.io.EOFException;
